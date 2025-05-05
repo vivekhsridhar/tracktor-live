6ceaace735210ed0e625f9b077cb84744bf26b58
Pranav 'Baba' Minasandra <pminasandra@ab.mpg.de>
2025-05-05 13:46:05 +0200

    Looming response, and command execution

--- examples/zy-looming-video/looming.py
+++ examples/zy-looming-video/looming.py
@@ -1,34 +1,48 @@
 
 """
-Example script to chunk video data only when animals approach close by.
+Example script to launch a looming video only when fish is moving.
 """
 
 import multiprocessing as mp
 import os
 from os.path import join as joinpath
+import subprocess
+import time
 mp.set_start_method('fork')
 
+import cv2
 import json
 import numpy as np
 import ulid
 
 import tracktorlive as trl
 
-# This demonstrates how to chunk input from video sources, retaining
-# only videos where animals are in proximity. Here we will use a video,
-# but this is only for demonstration. This is practically intended for
-# cases when interactions are rare and a real-time tracktorserver is run.
-# In this video example, we use Zerg Ultralisks engaging in combat, a common
-# scene from the video game Starcaft: Brood War (1998) to act as a proxy for
-# animal interactions.
+# This shows how to setup a server-client system to run a specific command, in
+# this case play a video, whenever the animal is moving. The video example is
+# motivated by presenting looming stimuli, but any shell command can be added in
+# its place, e.g., to run equipment, launch web services, etc. Likewise, any
+# condition other than velocity can also be programmed.
+# User params:
+VEL_CALC_NUM_FRAMES = 5
+THRESHOLD_VEL = 125 #px/s
+RUN_COMMAND = "cvlc --fullscreen --play-and-exit --no-osd ./looming-video.mp4"
+COMMAND_COOLDOWN = 5 #seconds
+# Code starts below.
+
+
+def run_quiet_command(cmd=RUN_COMMAND):
+    """Run a bash command quietly and block until it completes."""
+    print("starting command")
+    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, env=os.environ.copy())
+    print("stopping command")
+
 
 # First, let's load the parameters.
 with open("flume-video-params.json") as f:
     params = json.load(f)
 params["fps"] = 30.0
-#
-#THRESH_APPROACH_DIST = 300 #px
-MIN_RECORDED_ITERS = 30
+
+
 #os.makedirs("ultralisks-chunked", exist_ok=True)
 #
 
@@ -39,51 +53,48 @@ server, semm = trl.spawn_trserver("./flume_video.mp4",
                                     buffer_size=2#just need a second before interactions
                                 )
 
-## We will now write a function to do the chunking based on this distance:
-@server#adding this decorator makes this a server-side casette
-def print_vel(server):
-    data, clock = server.get_data_and_clock()
+# First, we need to mask out everyhthing outside region of interest
+@server#makes this a server-side casette
+def mask(server):
+    frame = server.current_frame
+    mask = np.zeros(frame.shape)
+    mask = cv2.rectangle(mask, (280, 3), (1030,690), (255,255,255), -1)
+    frame[mask ==  0] = 0
 
-    nonnandat = sum(data[~np.isnan(data)])
-    if nonnandat < MIN_RECORDED_ITERS*2:
-        return None
-    data = data[:,:,-MIN_RECORDED_ITERS:]
-    clock = clock[-MIN_RECORDED_ITERS:]
-    if np.any(data < 0):
-        return None
+@server#uncomment to show video on screen
+def show(server):
+    if server.current_frame is None:
+        return
+    cv2.imshow('tracking', server.current_frame)
+    cv2.waitKey(1)
+
+client = trl.spawn_trclient(server.feed_id)
+
+time_last = mp.Value('d', 0.0)
+
+## We will now write a function to do the velocity based video response on this distance:
+@client#adding this decorator makes this a client-side casette
+def average_speed(data, clock):
+    # Extract recent data
+    coords = data[0, :, -VEL_CALC_NUM_FRAMES:]  # shape (2, VEL_CALC_NUM_FRAMES)
+    times = clock[-VEL_CALC_NUM_FRAMES:]
+
+    # Skip if any coordinates or timestamps are invalid
+    if np.isnan(coords).any() or np.isnan(times).any():
+        return
+
+    # Compute displacements and distances
+    diffs = np.diff(coords, axis=1)  # shape (2, VEL_CALC_NUM_FRAMES-1)
+    dists = np.linalg.norm(diffs, axis=0)  # shape (VEL_CALC_NUM_FRAMES-1,)
+
+    dt = times[-1] - times[0]
+    if dt > 0:
+        avg_speed = np.sum(dists) / dt
     else:
-        dX_Y = data[:,:,-1] - data[:,:,-2]
-        dR = (dX_Y**2).sum()
-        v = dR/(clock[-1] - clock[-2])
+        return
 
-        print(dR, "\n")
+    if avg_speed > THRESHOLD_VEL and time.time() - time_last.value > COMMAND_COOLDOWN:
+        time_last.value = time.time()
+        run_quiet_command()
 
-
-#def chunking(server):
-#    data, _ = server.get_data_and_clock()
-#    curr_vals = data[:,:,-1]
-#
-#    if np.any(curr_vals < 0.0):
-#        # we don't know the location of some individuals
-#        return None
-#
-#    dist = np.sqrt(((curr_vals[0,:] - curr_vals[1,:])**2).sum())
-#    if dist <= THRESH_APPROACH_DIST:
-#        if len(server.recorded_frames) == 0:#nothing stored yet?
-#            server.keep_video.value = True
-#    elif dist > THRESH_APPROACH_DIST:
-#        if len(server.recorded_frames) > 0:
-#            server.keep_video.value=False
-#            fname = f'chunk-{ulid.ULID()}.avi'
-#            server.dumpvideo(joinpath('ultralisks-chunked', fname))
-#
-## Since this is recording from a video, we need to handle end-of-file events
-## However, this bit isn't relevant in real-time applications.
-#@server.stopfunc#adding this decorator makes it a server-side termination functionality
-#def final_chunk(server):
-#    if len(server.recorded_frames) > 0:
-#        server.keep_video.value=False
-#        fname = f'chunk-{ulid.ULID()}.avi'
-#        server.dumpvideo(joinpath('ultralisks-chunked', fname))
-#
-trl.run_trserver(server, semm)
+trl.run_trsession(server, semm, client)
